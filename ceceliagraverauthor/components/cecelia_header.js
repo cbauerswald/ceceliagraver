385fadf1f62c0fc79acdc8475a7f180f86b76010
Cecelia Auerswald <cecelia.auerswald@gmail.com>
2023-11-20 16:32:36 -0500

    run codemod new link to update Link component

--- ceceliagraverauthor/components/cecelia_header.js
+++ ceceliagraverauthor/components/cecelia_header.js
@@ -1,23 +1,31 @@
 import Link from 'next/link';
 
 const CeceliaHeader = ({ selected }) => {
-  return <div className="header">
-      <Link href="/"><a className="relative">
-        <img className="" src="/tree.png" />
-        <h1 className="header-text">
-          Cecelia Auerswald
-      </h1>
-      <h2 className="font-body uppercase text-base">Nyc Based Actor</h2>
-      </a></Link>
-    <div className="navbar">
-      <Link href="/"><a className={`nav-item ${selected == "home" && "selected"}`}>Home</a></Link>
-      <Link href="/gallery"><a className={`nav-item ${selected == "gallery" && "selected"}`}>Gallery</a></Link>
-      <Link href="/reels"><a className={`nav-item ${selected == "reels" && "selected"}`}>Reels</a></Link>
-      <Link href="/resume"><a className={`nav-item hidden md:block ${selected == "resume" && "selected"}`} >Resume</a></Link>
-      <a className="nav-item md:hidden" href="/resume.pdf" target="_blank"> Resume</a>
-      <Link href="/contact"><a className={`nav-item ${selected == "contact" && "selected"}`}>Contact</a></Link>
+  return (
+    <div className="header">
+        <Link href="/" className="relative">
+          <img className="" src="/tree.png" />
+          <h1 className="header-text">
+            Cecelia Auerswald
+        </h1>
+          <h2 className="font-body uppercase text-base">Nyc Based Actor</h2>
+        </Link>
+      <div className="navbar">
+        <Link href="/" className={`nav-item ${selected == "home" && "selected"}`}>Home</Link>
+        <Link
+          href="/gallery"
+          className={`nav-item ${selected == "gallery" && "selected"}`}>Gallery</Link>
+        <Link href="/reels" className={`nav-item ${selected == "reels" && "selected"}`}>Reels</Link>
+        <Link
+          href="/resume"
+          className={`nav-item hidden md:block ${selected == "resume" && "selected"}`}>Resume</Link>
+        <a className="nav-item md:hidden" href="/resume.pdf" target="_blank"> Resume</a>
+        <Link
+          href="/contact"
+          className={`nav-item ${selected == "contact" && "selected"}`}>Contact</Link>
+      </div>
     </div>
-  </div>
+  );
 }
 
 export default CeceliaHeader;
