7fd2d252628c9773462b6f49580b2df7a40861de
Cecelia Auerswald <cecelia.auerswald@gmail.com>
2023-11-20 21:44:45 -0500

    removing document import from _app.js

--- ceceliagraverauthor/pages/_app.js
+++ ceceliagraverauthor/pages/_app.js
@@ -1,16 +1,10 @@
 //styles
 import '../scss/style.scss';
 import React from "react";
-import { Head } from 'next/document';
 // import 'react-image-lightbox/style.css';
 
 function MyApp({ Component, pageProps }) {
   return <div>
-    {/* <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <title>Cecelia Auerswald</title>
-    </Head> */}
     <Component {...pageProps} />
   </div>
 }
